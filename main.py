e9593c1e90a0a695d99d8ef842ebae4988003745
XanPrincipe <gwjesus0407@gmail.com>
2024-11-17 20:42:54 +0300

    Lesson 8 Visual Changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
-from pygame import KEYDOWN
-
+from pygame import KEYDOWN, KEYUP, MOUSEBUTTONUP, MOUSEBUTTONDOWN
 
 clock = pygame.time.Clock()     # Создаем переменную для задержки анимации
 pygame.init()   # Запуск программы, используется всегда как например в tkinter win = Tk()
@@ -26,16 +25,22 @@ walk_right = [
     pygame.image.load("images/player_right/right4.png").convert_alpha(),
 ]
 
+lose_bg = pygame.image.load("images/gameover.jpg").convert()
+
 # Добавляем врага
 enemy = pygame.image.load("images/enemy/enemy.png").convert_alpha() # Для png всегда писать convert_alpha, а для всех других можно просто convert
-
+# Добавляем патрон
+bullet = pygame.image.load("images/bullet/bullet.png").convert_alpha()
+bullets = []
 # Добавляем список с врагами
 enemy_list_in_game = []
 # Создаем счетчик анимаций
 player_anim_count = 0
+#
 
 # Создаем переменную для дальнейшего движения заднего фона
 bgx = 0
+bg_lose_x = 0
 
 player_speed = 12    # Скорость персонажа
 player_x = 150      # Координата персонажа по x
@@ -52,12 +57,17 @@ jump_height = 7     # Высота прыжка
 enemy_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(enemy_timer, 2000)
 
+ammo = 5
+
 # Подключаем шрифт и задаем параметры текста
 label = pygame.font.Font("fonts/SourGummy-VariableFont_wdth,wght.ttf", 40)
 lose_label = label.render("You Lost", False,(193,196,199))
 label_rect = lose_label.get_rect(center=(640, 360))
-restart_label = label.render("Play again", False,(115,132,148))
+restart_label = label.render("Play again [R]", False,("white"))
 restart_label_rect = restart_label.get_rect(center=(640, 460))
+ammo_left =label.render(f'Ammo Left: {ammo} ', False,(193,196,199))
+ammo_left_rect = ammo_left.get_rect(topright=(640, 460))
+
 
 gameplay = True
 
@@ -66,7 +76,7 @@ while running: # Аналог из tkinter win.mainloop()
 
     screen.blit(bg, (bgx, 0))
     screen.blit(bg, (bgx + 1280, 0))
-
+    screen.blit(ammo_left, (1, 1))
     if gameplay:
     # Создаем коллизию
         player_rect = walk_right[0].get_rect(topleft=(player_x, player_y))
@@ -122,20 +132,39 @@ while running: # Аналог из tkinter win.mainloop()
     # Условие движения экрана
         if bgx <= -1280:
             bgx = 0
+
+        ammo_left = label.render(f'Ammo Left: {ammo} ', False, ("black"))
+        ammo_left_rect = ammo_left.get_rect(topright=(640, 460))
+
+        if bullets:
+            for (i, el) in enumerate (bullets):
+                screen.blit(bullet, (el.x, el.y))
+                el.x += 4
+                if el.x > 1290:
+                    bullets.pop(i)
+
+                if enemy_list_in_game:
+                    for (index, enemy_el) in enumerate (enemy_list_in_game):
+                        if el.colliderect(enemy_el):
+                            enemy_list_in_game.pop(index)
+                            bullets.pop(i)
     else:
         # Вывод экрана проигрыша вместе с текстом
-        screen.fill((87, 88, 89))
-        screen.blit(lose_label, label_rect)
+        screen.blit(lose_bg, (bg_lose_x, 0))
         screen.blit(restart_label, restart_label_rect)
 
         # Условия рестарта
+        keys = pygame.key.get_pressed()
         mouse = pygame.mouse.get_pos()      # Этот метод присваивает координаты x, y переменной mouse
-        if restart_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:    # collidepoint - проверяет соприкосновение прямоугольника текста рестарта с мышью
+        if restart_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0] or keys[pygame.K_r]:    # collidepoint - проверяет соприкосновение прямоугольника текста рестарта с мышью
             gameplay = True
             player_x = 150
             player_y = 565
+            bullets.clear()
             enemy_list_in_game.clear()  # Чистим врагов
-    pygame.display.update()     # Обновляет изображения на экране
+            ammo = 5
+
+    pygame.display.flip()     # Обновляет изображения на экране
 
     for event in pygame.event.get():    # Получаем список из всех возможных событий
         if event.type == pygame.QUIT:   # Если мы нажимаем на кнопку закрытия, то закрывается цикл, соответственно закрывается программа
@@ -143,6 +172,9 @@ while running: # Аналог из tkinter win.mainloop()
             pygame.quit()
         if event.type == enemy_timer:   # Если событие enemy_timer, то добавляем колизию врага в список
            enemy_list_in_game.append(enemy.get_rect(topleft=(1290, 587)))
+        if gameplay and event.type == pygame.MOUSEBUTTONDOWN and ammo > 0:
+            bullets.append(bullet.get_rect(topleft=(player_x + 30,player_y + 10)))
+            ammo -= 1
 
 # Скоросить движения персонажа и также смены задника
     clock.tick(60)
